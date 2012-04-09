7e8d1405352daef4bfaaad024fd8702fec456e64
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2012-04-09 13:43:18 +0200

    domain update

--- app.rb
+++ app.rb
@@ -24,7 +24,7 @@ configure do
       :port           => 25,
       :user_name      => ENV['SENDGRID_USERNAME'],
       :password       => ENV['SENDGRID_PASSWORD'],
-      :domain         => ENV['SENDGRID_DOMAIN'],
+      :domain         => "kasiafrychel.pl",
       :authentication => :plain
     }
   end
